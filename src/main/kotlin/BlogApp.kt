273cfe613ef0afcfbeb8f61f4e8fc70b9e5eceb1
박철오(Cheoloh Park)/OCB개발팀/SKP <cheoloh.park@sk.com>
2018-08-09 15:04:04 +0900

    first

---
+++ src/main/kotlin/BlogApp.kt
@@ -0,0 +1,21 @@
+package blog
+
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun Application.module() {
+    
+}
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("My Example Blog", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
